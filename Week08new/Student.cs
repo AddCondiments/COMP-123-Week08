a6826d9bc7549b0519d2768e54109a68206e0ec8
AddCondiments <addcondiments@gmail.com>
2017-06-29 11:58:08 -0400

    Welp, I was too slow to actually finish it, but I fixed the getter and setter for studentID!

---
+++ Week08new/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week08new
+{
+
+    class Student : Person
+    {
+        //Instance Vatiables, or strings
+        private string _studentID;
+
+        public string studentID
+        {
+            get
+            {
+                return this._studentID;
+            }
+
+            set
+            {
+                this._studentID = value;
+            }
+        }
+
+        //Need a constructor to fix intital error for inheritance
+        public Student(string name, int age, string studentID)
+            :base (name, age)
+        {
+            
+        }
+    }
+}
