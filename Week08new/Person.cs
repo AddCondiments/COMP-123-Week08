a9c28f44a6420a51c33d914441f615e62e3d237b
AddCondiments <addcondiments@gmail.com>
2017-06-29 11:37:17 -0400

    First Method

---
+++ Week08new/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week08new
+{
+    /// <summary>
+    /// Person class for Tom's demo :^)
+    /// </summary>
+    class Person
+    {
+        //Private Instance Variables. or Fields
+
+        private string _name;
+        private int _age;
+        //Public properties
+        public string name {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+                }
+        public int Age {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                this._age = value;
+            }
+        }
+        /// <summary>
+        /// This following method will enable people to talk.
+        /// </summary>
+
+        public void Talks()
+        {
+            Console.WriteLine(this._name + " is talking!");
+        }
+
+    }
+}
